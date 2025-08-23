9fa87bb846c30cd1e2fbb3bdbbcde8c91a2daf19
Tin <dustinkyle122915@gmail.comgit config --global user.email dustinkylegarcia@gmail.com>
2025-08-23 22:25:26 +0800

    fixes the problem

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",            // main HTML file
+    "./src/**/*.{js,jsx,ts,tsx}" // all JS/TS/JSX/TSX files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
